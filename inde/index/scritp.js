eb686bcf6a23804cd206c1690bb269b3c8694547
michellis22 <michellispicanco@gmail.com>
2025-06-25 19:04:51 -0300

    minhas script

---
+++ inde/index/scritp.js
@@ -0,0 +1,6 @@
+function inserirNome () ( )
+let nomeusuario - prompt ("Qual o seu nome");
+let elemento - document.querySelector ("#Nome usuario");
+elemento.textoContext = nomeusuario;
+}
+inserirNome();
