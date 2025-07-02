797fb16bf6117dfd83c44ebb6a8d9f270536f7ec
michellis22 <michellispicanco@gmail.com>
2025-07-02 18:58:31 -0300

    criei lista

--- inde/index/scritp.js
+++ inde/index/scritp.js
@@ -4,3 +4,10 @@ let elemento - document.querySelector ("#Nome usuario");
 elemento.textoContext = nomeusuario;
 }
 inserirNome();
+
+let linguagens = ["JavaScript", "Python", "C++"];
+console.log(linguagens[1])
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[1];
+
