d621a816cbb7c80c8d9ad2f7dc79a3ca38af8345
michellis22 <michellispicanco@gmail.com>
2025-07-30 18:51:27 -0300

    exercicio

--- inde/index/scritp.js
+++ inde/index/scritp.js
@@ -12,3 +12,11 @@ const item = document.querySelector("#lista");
 item.textContent = linguagens[1];
 item.textoContext[0] +
 
+let aluno1 = {
+    nome: "Gabriel",
+    idade: 13;
+    anoletivo: "2ªsérie",
+    materiasFavoritas: ["Geografia", "Programação", "Arte"]
+}
+const objeto = document.querySelector()"#aluno");
+objeto.textContent = aluno1.nome;
